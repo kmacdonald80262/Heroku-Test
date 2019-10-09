abe353560705c3940d417e2ebacf0d9f2170161c
Kathleen MacDonald <k_macdonald@fanshaweonline.ca>
2019-10-08 22:38:41 -0400

    'basic-framework'

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,29 @@
-(() => {
-    // test fired
-    console.log('fired!');
-})()
+(function(){
+	"use strict";
+	
+	console.log("fired!!!");
+
+	var button = document.querySelector("#button");
+	var burgerCon = document.querySelector("#burgerCon");
+
+	function hamburgerMenu() {
+		burgerCon.classList.toggle("slideToggle");
+		button.classList.toggle("expanded");
+	}
+
+	function closeMenu() {
+
+		if (window.matchMedia("(min-width: 1024px)").matches && burgerCon.classList.contains('slideToggle') ) {
+		console.log("media query fired");
+		burgerCon.classList.remove('slideToggle');
+		button.classList.remove('expanded');
+		}
+
+	}
+
+	button.addEventListener("click", hamburgerMenu, false);
+
+	window.addEventListener("resize", closeMenu);
+
+		
+})();
